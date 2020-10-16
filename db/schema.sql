10571f465d2c95803a821b2f711c1773c52bc021
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-16 13:04:22 -0500

    initial files and database set up

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT(10) AUTO_INCREMENT,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Swiss", true),
+("Turkey Burger", false),
+("Impossible Burger", false),
+("Patty Mellt", true);
