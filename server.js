51277693795d81699ff84a63ce5535c7ac4dbfed
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-22 14:01:25 -0500

    timeout

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ let routes = require("./controllers/burger_controller");
 
 app.use(routes);
 
+// Timeout
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
+
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
