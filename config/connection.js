0457e26c85eb9ef1b7e8bb366838e2ce6d58ba5a
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-22 13:17:59 -0500

    still working on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,11 @@
 // Set up MySQL connection
 const mysql = require("mysql");
 let connection;
-let jaws = "mysql://j6srevpas0mf8h4t:rexruike78epl9kb@vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qtbvbg9c055pszui"
-
 
 console.log(process.env.JAWSDB_URL)
 
-if (jaws) {
-  connection = mysql.createConnection(jaws);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
@@ -19,13 +17,8 @@ if (jaws) {
 };
 
 // Make connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+
 
 // Export connection for ORM to use
 module.exports = connection;
