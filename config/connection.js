a4235c617cc9bce153cea4f0293d1403a0701d48
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-22 13:38:37 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Sriracha96$",
+    password: process.env.mypass,
     database: "burgers_db"
   });
 };
